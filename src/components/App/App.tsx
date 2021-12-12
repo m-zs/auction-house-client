fbf76665dbcdcd90084e3559b31eab8de945fa28
m-zs <mzasada93@gmail.com>
2021-12-12 19:01:15 +0100

    feat: add graphql setup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,41 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
 import "./App.css";
 
-export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const users = gql`
+  query Users {
+    users {
+      items {
+        id
+        username
+        status
+        role
+      }
+    }
+  }
+`;
+
+export const App = () => {
+  const { loading, error, data } = useQuery(users);
+
+  console.log(data, error, loading);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import "./index.css";
 import { App } from "./components/App";
 
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
