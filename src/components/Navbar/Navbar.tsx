bb289598bf11a46c2dedb18c06413c556b033a8c
m-zs <mzasada93@gmail.com>
2022-01-09 10:20:47 +0100

    refactor: move items to utils, add login validation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { Container } from "@mui/material";
+import { useReactiveVar } from "@apollo/client";
 
+import { userVar } from "state/vars";
 import * as S from "./styles";
+import { NAV_PAGES } from "./items";
 
-const pages = [
-  { url: "/", text: "Home" },
-  { url: "/sign-up", text: "Sign Up" },
-  { url: "/sign-in", text: "Sign In" },
-];
+const Navbar = () => {
+  const user = useReactiveVar(userVar);
 
-const Navbar = () => (
-  <S.Container>
-    <S.StyledAppBar position="sticky">
-      <Container>
-        <S.InnerContainer>
-          {pages.map(({ text, url }) => (
-            <S.StyledNavLink to={url} key={text}>
-              {text}
-            </S.StyledNavLink>
-          ))}
-        </S.InnerContainer>
-      </Container>
-    </S.StyledAppBar>
-  </S.Container>
-);
+  const items = NAV_PAGES.filter(
+    ({ hideForUser, onlyForLoggedIn }) =>
+      (user && !hideForUser) || (!user && !onlyForLoggedIn)
+  );
+
+  return (
+    <S.Container>
+      <S.StyledAppBar position="sticky">
+        <Container>
+          <S.InnerContainer>
+            {items.map(({ text, url }) => (
+              <S.StyledNavLink to={url} key={text}>
+                {text}
+              </S.StyledNavLink>
+            ))}
+          </S.InnerContainer>
+        </Container>
+      </S.StyledAppBar>
+    </S.Container>
+  );
+};
 
 export default Navbar;
---
+++ src/components/Navbar/items.ts
@@ -0,0 +1,16 @@
+interface NavPage {
+  /** page url */
+  url: string;
+  /** anchor text displayed */
+  text: string;
+  /** protected page, visible only for logged in users */
+  onlyForLoggedIn?: boolean;
+  /** page only for not logged in users */
+  hideForUser?: boolean;
+}
+
+export const NAV_PAGES: NavPage[] = [
+  { url: "/", text: "Home" },
+  { url: "/sign-up", text: "Sign Up", hideForUser: true },
+  { url: "/sign-in", text: "Sign In", hideForUser: true },
+];
